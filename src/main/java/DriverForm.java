921e89d20ddefd4ac4fb364356f8177be360aba0
Brandan <brandan.jones@uc.edu>
2022-10-06 22:07:49 -0400

    Gather data from a Swing form with JTextField, JComboBox, and JButton. See how I did it in this video: https://youtu.be/1IgeJYDuS_8

--- src/main/java/DriverForm.java
+++ src/main/java/DriverForm.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DriverForm {
     private JPanel pnlMain;
@@ -14,6 +16,42 @@ public class DriverForm {
     private JTextField txtOdometer;
     private JTextField txtMilesPerGallon;
     private JTextField txtGallonsOfGas;
+
+    public DriverForm() {
+
+        initializeVehicleTypeComboBox();
+        btnSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String strOdometer = txtOdometer.getText();
+                int odometer = Integer.parseInt(strOdometer);
+
+                String strMilesPerGallon = txtMilesPerGallon.getText();
+                int milesPerGallon = Integer.parseInt(strMilesPerGallon);
+
+                String strGallonsOfGas = txtGallonsOfGas.getText();
+                double gallonsOfGas = Double.parseDouble(strGallonsOfGas);
+
+                String type = cmbMakeModel.getSelectedItem().toString();
+                Vehicle vehicle = Driver.getInstance().createVehicle(type);
+
+                vehicle.setOdometer(odometer);
+                vehicle.setGallonsOfGas(gallonsOfGas);
+                vehicle.setMilesPerGallon(milesPerGallon);
+
+            }
+        });
+    }
+
+    private void initializeVehicleTypeComboBox() {
+        DefaultComboBoxModel<String> vehicleTypesModel = new DefaultComboBoxModel<>();
+        vehicleTypesModel.addElement(Driver.SONIC);
+        vehicleTypesModel.addElement(Driver.PRIUS);
+        vehicleTypesModel.addElement(Driver.MUSTANG);
+        cmbMakeModel.setModel(vehicleTypesModel);
+
+
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("DriverForm");
